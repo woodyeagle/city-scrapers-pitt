4622b4e2f04d89a549b21d1557dc67af96daccb4
Will Snavely <will.snavely@protonmail.com>
2020-09-18 18:07:52 -0400

    Update test_bethel_park_public_meetings.py
    Trying again to clear CI, to see if my lockfile is reasonable.

--- tests/test_bethel_park_public_meetings.py
+++ tests/test_bethel_park_public_meetings.py
@@ -11,11 +11,11 @@ from city_scrapers.spiders.bethel_park_public_meetings import BethelParkSpider
 test_response = file_response(
     join(dirname(__file__), "files", "bethel_park", "bethel_park_public_meetings.ics"),
     url="http://bethelpark.net/?"
-        "plugin=all-in-one-event-calendar"
-        "&controller=ai1ec_exporter_controller"
-        "&action=export_events"
-        "&ai1ec_cat_ids=42"
-        "&no_html=true"
+    "plugin=all-in-one-event-calendar"
+    "&controller=ai1ec_exporter_controller"
+    "&action=export_events"
+    "&ai1ec_cat_ids=42"
+    "&no_html=true"
 )
 spider = BethelParkSpider()
 
@@ -42,8 +42,10 @@ def test_titles():
         "Council Committee Meeting", "Shade Tree Commission Meeting",
         "Shade Tree Commission Meeting", "Zoning Hearing Board",
         "Planning & Zoning Commission Workshop Meeting",
-        ("Municipal Council Committee Meeting July 27th, 2020 7:30 PM "
-         "– Public Hearings starting at 6:30 PM")
+        (
+            "Municipal Council Committee Meeting July 27th, 2020 7:30 PM "
+            "– Public Hearings starting at 6:30 PM"
+        )
     ]
     for (event, expected_title) in zip(get_test_sample(), expected_titles):
         assert event["title"] == expected_title
